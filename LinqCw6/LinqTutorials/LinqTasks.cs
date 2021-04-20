2efc51c042318f82556c0b4eaf1f7d2911a9760a
tomekkonopko <61691104+tomekkonopko@users.noreply.github.com>
2021-04-20 17:03:55 +0200

    Add files via upload

---
+++ LinqCw6/LinqTutorials/LinqTasks.cs
@@ -0,0 +1,378 @@
+﻿using LinqTutorials.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqTutorials
+{
+    public static class LinqTasks
+    {
+        public static IEnumerable<Emp> Emps { get; set; }
+        public static IEnumerable<Dept> Depts { get; set; }
+
+        static LinqTasks()
+        {
+            var empsCol = new List<Emp>();
+            var deptsCol = new List<Dept>();
+
+            #region Load depts
+
+            var d1 = new Dept
+            {
+                Deptno = 1,
+                Dname = "Research",
+                Loc = "Warsaw"
+            };
+
+            var d2 = new Dept
+            {
+                Deptno = 2,
+                Dname = "Human Resources",
+                Loc = "New York"
+            };
+
+            var d3 = new Dept
+            {
+                Deptno = 3,
+                Dname = "IT",
+                Loc = "Los Angeles"
+            };
+
+            deptsCol.Add(d1);
+            deptsCol.Add(d2);
+            deptsCol.Add(d3);
+            Depts = deptsCol;
+
+            #endregion
+
+            #region Load emps
+
+            var e1 = new Emp
+            {
+                Deptno = 1,
+                Empno = 1,
+                Ename = "Jan Kowalski",
+                HireDate = DateTime.Now.AddMonths(-5),
+                Job = "Backend programmer",
+                Mgr = null,
+                Salary = 2000
+            };
+
+            var e2 = new Emp
+            {
+                Deptno = 1,
+                Empno = 20,
+                Ename = "Anna Wesolowska",
+                HireDate = DateTime.Now.AddMonths(-7),
+                Job = "Frontend programmer",
+                Mgr = e1,
+                Salary = 4000
+            };
+
+            var e3 = new Emp
+            {
+                Deptno = 1,
+                Empno = 2,
+                Ename = "Maciek Gortat",
+                HireDate = DateTime.Now.AddMonths(-3),
+                Job = "Frontend programmer",
+                Mgr = null,
+                Salary = 5000
+            };
+
+            var e4 = new Emp
+            {
+                Deptno = 2,
+                Empno = 3,
+                Ename = "Pan Paweł",
+                HireDate = DateTime.Now.AddMonths(-2),
+                Job = "Frontend programmer",
+                Mgr = e2,
+                Salary = 5500
+            };
+
+            var e5 = new Emp
+            {
+                Deptno = 2,
+                Empno = 4,
+                Ename = "Krzysztof Krawczyk(R.I.P)",
+                HireDate = DateTime.Now.AddMonths(-2),
+                Job = "Backend programmer",
+                Mgr = e2,
+                Salary = 5500
+            };
+
+            var e6 = new Emp
+            {
+                Deptno = 2,
+                Empno = 5,
+                Ename = "Poka Pokahontaz",
+                HireDate = DateTime.Now.AddMonths(-3),
+                Job = "Manager",
+                Mgr = null,
+                Salary = 8000
+            };
+
+            var e7 = new Emp
+            {
+                Deptno = null,
+                Empno = 6,
+                Ename = "Rambo Rambowski",
+                HireDate = DateTime.Now.AddMonths(-3),
+                Job = "System administrator",
+                Mgr = null,
+                Salary = 7500
+            };
+
+            var e8 = new Emp
+            {
+                Deptno = 2,
+                Empno = 7,
+                Ename = "Pustynia Puszcza",
+                HireDate = DateTime.Now.AddMonths(-3),
+                Job = "Mobile developer",
+                Mgr = null,
+                Salary = 4000
+            };
+
+            var e9 = new Emp
+            {
+                Deptno = 2,
+                Empno = 8,
+                Ename = "Józef Piłsudski",
+                HireDate = DateTime.Now.AddMonths(-9),
+                Job = "CTO",
+                Mgr = null,
+                Salary = 12000
+            };
+
+            var e10 = new Emp
+            {
+                Deptno = 2,
+                Empno = 9,
+                Ename = "Andrzej Duda",
+                HireDate = DateTime.Now.AddMonths(-4),
+                Job = "Database administrator",
+                Mgr = e95,
+                Salary = 9000
+            };
+
+
+            empsCol.Add(e1);
+            empsCol.Add(e2);
+            empsCol.Add(e3);
+            empsCol.Add(e4);
+            empsCol.Add(e5);
+            empsCol.Add(e6);
+            empsCol.Add(e7);
+            empsCol.Add(e8);
+            empsCol.Add(e9);
+            empsCol.Add(e10);
+            Emps = empsCol;
+
+            #endregion
+        }
+
+        /// <summary>
+        ///     SELECT * FROM Emps WHERE Job = "Backend programmer";
+        /// </summary>
+        public static IEnumerable<Emp> Task1()
+        {
+            return Emps.Where(x => x.Job == "Backend programmer").ToList();
+        }
+
+        /// <summary>
+        ///     SELECT * FROM Emps Job = "Frontend programmer" AND Salary>1000 ORDER BY Ename DESC;
+        /// </summary>
+        public static IEnumerable<Emp> Task2()
+        {
+            IEnumerable<Emp> result = null;
+
+            result = Emps.Where(x => x.Job == "Frontend programmer").Where(x => x.Salary > 1000)
+                .OrderByDescending(x => x.Ename).ToList();
+
+            return result;
+        }
+
+
+        /// <summary>
+        ///     SELECT MAX(Salary) FROM Emps;
+        /// </summary>
+        public static int Task3()
+        {
+            return Emps.Max(x => x.Salary);
+        }
+
+        /// <summary>
+        ///     SELECT * FROM Emps WHERE Salary=(SELECT MAX(Salary) FROM Emps);
+        /// </summary>
+        public static IEnumerable<Emp> Task4()
+        {
+            return Emps.Where(x => x.Salary == (Emps.Max(y => y.Salary))).ToList(); ;
+        }
+
+        /// <summary>
+        ///    SELECT ename AS Nazwisko, job AS Praca FROM Emps;
+        /// </summary>
+        public static IEnumerable<object> Task5()
+        {
+            return Emps.Select(e => new { Nazwisko = e.Ename, Praca = e.Job });
+        }
+
+        /// <summary>
+        ///     SELECT Emps.Ename, Emps.Job, Depts.Dname FROM Emps
+        ///     INNER JOIN Depts ON Emps.Deptno=Depts.Deptno
+        ///     Rezultat: Złączenie kolekcji Emps i Depts.
+        /// </summary>
+        public static IEnumerable<object> Task6()
+        {
+            IEnumerable<object> result = Emps.Join(Depts, emp => emp.Deptno, dept => dept.Deptno, ((emp, dept) => new
+            {
+                Ename = emp.Ename,
+                Job = emp.Job,
+                Dname = dept.Dname
+            })).ToList();
+            
+            return result;
+        }
+
+        /// <summary>
+        ///     SELECT Job AS Praca, COUNT(1) LiczbaPracownikow FROM Emps GROUP BY Job;
+        /// </summary>
+        public static IEnumerable<object> Task7()
+        {
+            return Emps.GroupBy(x => x.Job).Select(n => new
+            {
+                Praca = n.Key,
+                Count = n.Count()
+            }).ToList();
+        }
+
+        /// <summary>
+        ///     Zwróć wartość "true" jeśli choć jeden
+        ///     z elementów kolekcji pracuje jako "Backend programmer".
+        /// </summary>
+        public static bool Task8()
+        {
+            return Emps.Where(e => e.Job == "Backend programmer")
+                .Any();
+        }
+
+        /// <summary>
+        ///     SELECT TOP 1 * FROM Emp WHERE Job="Frontend programmer"
+        ///     ORDER BY HireDate DESC;
+        /// </summary>
+        public static Emp Task9()
+        {
+            return Emps.Where(emp => emp.Job == "Frontend programmer").OrderByDescending(x => x.HireDate).First(); ;
+        }
+
+        /// <summary>
+        ///     SELECT Ename, Job, Hiredate FROM Emps
+        ///     UNION
+        ///     SELECT "Brak wartości", null, null;
+        /// </summary>
+        public static IEnumerable<object> Task10()
+        {
+            return Emps.Select(emp => new
+            {
+                Ename = emp.Ename,
+                Job = emp.Job,
+                Hiredate = emp.HireDate
+            }).Union(Emps.Select(x => new
+            {
+                Ename = "Brak wartości",
+                Job = (string)null,
+                Hiredate = (DateTime?)null
+            })).ToList();
+        }
+
+        /// <summary>
+        /// Wykorzystując LINQ pobierz pracowników podzielony na departamenty pamiętając, że:
+        /// 1. Interesują nas tylko departamenty z liczbą pracowników powyżej 1
+        /// 2. Chcemy zwrócić listę obiektów o następującej srukturze:
+        ///    [
+        ///      {name: "RESEARCH", numOfEmployees: 3},
+        ///      {name: "SALES", numOfEmployees: 5},
+        ///      ...
+        ///    ]
+        /// 3. Wykorzystaj typy anonimowe
+        /// </summary>
+        public static IEnumerable<object> Task11()
+        {
+            return Emps.Join(Depts, e => e.Deptno, d => d.Deptno, (e, d) => new { Emps = e, Depts = d })
+                            .GroupBy(g => g.Depts)
+                            .Select(r => new
+                            {
+                                name = r.Key.Dname,
+                                numOfEmployees =
+                                Emps.Where(n => n.Deptno == r.Key.Deptno).Count()
+                            })
+                            .Where(o => o.numOfEmployees > 1);
+        }
+
+        /// <summary>
+        /// Napisz własną metodę rozszerzeń, która pozwoli skompilować się poniższemu fragmentowi kodu.
+        /// Metodę dodaj do klasy CustomExtensionMethods, która zdefiniowana jest poniżej.
+        /// 
+        /// Metoda powinna zwrócić tylko tych pracowników, którzy mają min. 1 bezpośredniego podwładnego.
+        /// Pracownicy powinny w ramach kolekcji być posortowani po nazwisku (rosnąco) i pensji (malejąco).
+        /// </summary>
+        public static IEnumerable<Emp> Task12()
+        {
+            return Emps.GetEmpsWithSubordinates();
+        }
+
+        /// <summary>
+        /// Poniższa metoda powinna zwracać pojedyczną liczbę int.
+        /// Na wejściu przyjmujemy listę liczb całkowitych.
+        /// Spróbuj z pomocą LINQ'a odnaleźć tę liczbę, które występuja w tablicy int'ów nieparzystą liczbę razy.
+        /// Zakładamy, że zawsze będzie jedna taka liczba.
+        /// Np: {1,1,1,1,1,1,10,1,1,1,1} => 10
+        /// </summary>
+        public static int Task13(int[] arr)
+        {
+            return arr.GroupBy(i =>
+            new { Key = i, Count = arr.Where(n => n == i).Count() })
+                .Where(i2 => i2.Key.Count % 2 == 1)
+                .Select(i2 => i2.Key.Key)
+                .First();
+        }
+
+        /// <summary>
+        /// Zwróć tylko te departamenty, które mają 5 pracowników lub nie mają pracowników w ogóle.
+        /// Posortuj rezultat po nazwie departament rosnąco.
+        /// </summary>
+        public static IEnumerable<Dept> Task14()
+        {
+            return Depts.GroupJoin(Emps, d => d.Deptno, e => e.Deptno, (d, e) =>
+            new { Depts = d, Emps = e })
+                .GroupBy(g => g.Depts)
+                .Where(d =>
+                {
+                    int count = Emps.Where(n => n.Deptno == d.Key.Deptno)
+                    .Count();
+                    return count == 5 || count == 0;
+                })
+                .Select(d => d.Key)
+                .OrderBy(d => d.Dname);
+        }
+    }
+
+    public static class CustomExtensionMethods
+    {
+        //Put your extension methods here
+        public static IEnumerable<Emp> GetEmpsWithSubordinates(this IEnumerable<Emp> emps)
+        {
+            var result = emps.Where(e => emps.Any(e2 => e2.Mgr == e))
+                .OrderBy(e =>
+                {
+                    string lastName = e.Ename.Split(" ")[1];
+                    return lastName;
+                })
+                .ThenByDescending(e => e.Salary);
+            return result;
+        }
+
+    }
+}
